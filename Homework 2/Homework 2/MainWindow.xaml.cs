d328a9a4137815f520d7681ae5d2d2a0d72e4c57
chrisgraves <chrisgit53@hotmail.com>
2016-02-26 17:20:59 -0500

    Windowing System HW 2
    Windowing Systems Programming assignment:
    
    Outline:
    In this assignment, students will create a GUI that displays several
    radio buttons, a check button
    and a normal push-button.
    
    Interface:
    Students should create a GUI containing the following elements:
    • three radio buttons in the top-left of the window
    • three radio buttons in the top-right of the window, in a different
    group
    • a check-box in the bottom-left corner of the window
    • a push-button in the bottom-right corner of the window
    Each component should maintain its positioning as the window is resized,
    and none of them
    should be stretched out as the window resizes.
    
    Events:
    Students should arrange it so that the check-box controls whether or not
    the push-button is
    enabled. (When the check-box is checked, the push-button should be
    enabled; when the check-box
    is unchecked, the push-button should be disabled.)
    Students should arrange it so that, when the push-button is clicked, the
    application prints out
    which radio-buttons are selected. (Bear in mind that there are two
    groups, so two radio buttons
    might be selected. Also, depending on how your GUI is set up, it might
    be possible for no radio
    buttons to be selected.)
    Students should include a comment in each callback, stating what event
    on what object the
    callback is attached to, and what class the callback is a member-method
    of.

---
+++ Homework 2/Homework 2/MainWindow.xaml.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Homework_2
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        String selection1 = null;
+        String selection2 = null;
+
+        //Callback is attached to checkbox when it is checked
+        private void checkBox_Checked(object sender, RoutedEventArgs e)
+        {
+            button.IsEnabled = true;
+        }
+
+        //Callback is attached to checkbox when it is unchecked
+        private void checkBox_Unchecked(object sender, RoutedEventArgs e)
+        {
+            button.IsEnabled = false;
+        }
+
+        //Callback is attached to selection of specific radio button
+        private void radioButton_Checked(object sender, RoutedEventArgs e)
+        {
+            selection1 = "Radio Button 1 Selected";
+        }
+
+        //Callback is attached to selection of radio button
+        private void radioButton1_Checked(object sender, RoutedEventArgs e)
+        {
+            selection1 = "Radio Button 2 Selected";
+        }
+
+        //Callback is attached to selection of radio button
+        private void radioButton2_Checked(object sender, RoutedEventArgs e)
+        {
+            selection1 = "Radio Button 3 Selected";
+        }
+
+        //Callback is attached to selection of radio button
+        private void radioButton3_Checked(object sender, RoutedEventArgs e)
+        {
+            selection2 = "Radio Button 4 Selected\r\n";
+        }
+
+        //Callback is attached to selection of radio button
+        private void radioButton4_Checked(object sender, RoutedEventArgs e)
+        {
+            selection2 = "Radio Button 5 Selected\r\n";
+        }
+        
+        //Callback is attached to selection of radio button
+        private void radioButton5_Checked(object sender, RoutedEventArgs e)
+        {
+            selection2 = "Radio Button 6 Selected\r\n";
+        }
+
+        //Callback is attached to button click
+        private void button_Click(object sender, RoutedEventArgs e)
+        {
+            System.Console.WriteLine(selection1);
+            System.Console.WriteLine(selection2);
+            
+        }
+    }
+}
