3e381b147b136faa0671e07daf91193a7053741d
chrisgraves <chrisgit53@hotmail.com>
2016-02-28 09:38:15 -0500

    Windowing System HW 4
    Windowing Systems Programming assignment:
    
    Interface:
    Students should create a GUI containing the following elements:
    • a combo-box and a button in the top-let corner
    • a combo-box and a button in the top-right corner
    • a tab manager in the bottom-left corner
    • a stack-panel in the bottom-right corner
    There should not be any wasted space in the interface; the tab manager
    and stack-panel should
    expand to take up any space that is added to the layout as the GUI is
    resized. (The tab manager and
    stack-panel do not have to be equally sized; one could have a fixed
    width while the other resized
    freely, for example.)
    The combo-box in the top-left corner will be used to select between the
    tab-manager and stack
    panel, while the combo-box in the top-right corner will be used to
    select between a message-box
    and save-file dialog. Each should have appropriate values.
    
    Events:
    Students should arrange it so that, when the button next to the top-left
    combo box (the “combobox
    or stack panel” box) is clicked, one of two things happens, depending on
    the selected value in
    the combo-box:
    • if the tab manager is selected, then a new page is added to the tab
    manager. For the content
    of the new page, the student should use a text box wrapped in a
    scroll-viewer.
    • If the stack-panel is selected, then a new item is added to the stack
    panel. For a new item,
    the student should use a label
    Students should arrange it so that, when the button next to the
    top-right combo box (the “message
    or dialog” box) is clicked, one of two things happens, depending on the
    selected value in the
    combo-box:
    • if “message box” is selected, then a message dialog-box should be
    displayed. The message
    box should have at least two options, say “cancel” and “OK”, and the
    program should print
    which is selected.
    • If “file dialog” is selected, then a file dialog-box should be
    displayed. If the user selects a
    file, the file-path should be printed.

---
+++ Homework 4/Homework 4/MainWindow.xaml.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Homework_4
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        int w;
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+
+        private void comboBox_Loaded(object sender, RoutedEventArgs e)
+        {
+            List<string> disp_select = new List<string>();
+            disp_select.Add("Tab Manager");
+            disp_select.Add("Stack Panel");
+
+            var comboBox = sender as ComboBox;
+            comboBox.ItemsSource = disp_select;
+        }
+
+        private void comboBox1_Loaded(object sender, RoutedEventArgs e)
+        {
+            List<string> output_select = new List<string>();
+            output_select.Add("Message Box");
+            output_select.Add("File Dialog");
+
+            var comboBox1 = sender as ComboBox;
+            comboBox1.ItemsSource = output_select;
+        }
+
+        private void comboBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            var comboBox = sender as ComboBox;
+            int sel_value = comboBox.SelectedIndex;
+
+            if(sel_value == 0)
+            {
+                tabControl.IsEnabled = true;
+                stack.IsEnabled = false;
+            }
+            else if (sel_value == 1)
+            {
+                tabControl.IsEnabled = false;
+                stack.IsEnabled = true;
+            }
+        }
+
+        private void comboBox1_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            var comboBox1 = sender as ComboBox;
+            int sel_value1 = comboBox1.SelectedIndex;
+
+            if(sel_value1 == 0)
+            {
+                w = 1;
+            }
+            else if (sel_value1 == 1)
+            {
+                w = 2;
+            }
+        }
+
+        private void button_Click(object sender, RoutedEventArgs e)
+        {
+           
+            if (tabControl.IsEnabled == true)
+            {
+                TabItem t = new TabItem();
+
+                ScrollViewer view = new ScrollViewer();
+                view.Content = new TextBox();
+
+                t.Header = "New Header";
+                t.Content = view;
+                tabControl.Items.Add(t);
+             }
+            else if (stack.IsEnabled == true)
+            {
+                Label l = new Label();
+                l.Content = "New Label";
+                stack.Children.Add(l);
+            }
+        }
+
+        private void button1_Click(object sender, RoutedEventArgs e)
+        {
+            if (w == 1)
+            {
+                MessageBoxResult result = MessageBox.Show("New Message Box","New Box",MessageBoxButton.OKCancel);
+                Console.WriteLine(result.ToString());
+            }
+            else if (w == 2)
+            {
+                Microsoft.Win32.SaveFileDialog sfd = new Microsoft.Win32.SaveFileDialog();
+
+                bool? picked = sfd.ShowDialog();
+
+                if(picked == true)
+                {
+                    MessageBox.Show("You Picked: " + sfd.FileName, "File", MessageBoxButton.OK);
+                }
+               
+            }
+        }
+    }
+}
